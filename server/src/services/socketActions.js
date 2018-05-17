42dae7d3ca8cfb1028c2f025a14451ac33c4ab6c
Alex Alikiotis <alexalikiotis5@gmail.com>
2018-05-17 17:19:25 +0300

    Handling socket connections in server

--- server/src/services/socketActions.js
+++ server/src/services/socketActions.js
@@ -1,8 +1,90 @@
+import _ from 'lodash';
+import { Map, List } from 'immutable';
+
+import { Cards } from '../util/poker';
+
 class Socket {
 
     static actions(io) {
+
+        let clients = 0;
+        let initialState = Map({
+            deck: Cards,
+            deckIndex: 0,
+            pot: 0,
+            winner: null,
+            turn: null,
+            players: List()
+        });
+        let state = initialState;
         
         io.on('connection', (socket) => {
+
+            if (++clients < 2) {
+
+                sockert.emit('table_is_joinable');
+
+            } else {
+                socket.emit('table_is_full');
+                socket.disconnect();
+            }
+
+            socket.on('join', socket => {
+                
+                state = state.updateIn(['players'], players => players.push(Map({
+                    id: socket.id,
+                    next: false,
+                    hand: List(),
+                    money: 1500,
+                    bet: 0
+                })));
+
+                if (clients == 2) {
+
+                    state = state
+                        .set('deck', _.shuffle(state.get('deck')))
+                        .setIn(['players', 0], _.slice(state.get('deck'), 0, 5))
+                        .setIn(['players', 1], _.slice(state.get('deck'), 5, 10))
+                        .set('turn', 0)
+                        .set('deckIndex', 10);
+
+                    io.sockets.connected[state.getIn(['players', 0]).id].emit('server_state', {
+                        turn: state.getIn(['players', state.get('turn')]).id,
+                        players: {
+                            me: {
+                                hand: state.getIn(['players', 0]).hand,
+                                money: state.getIn(['players', 0]).money
+                            },
+                            opponent: {
+                                money: state.getIn(['players', 1]).money
+                            }
+                        }
+                    });
+
+                    io.sockets.connected[state.getIn(['players', 1]).id].emit('server_state', {
+                        turn: state.getIn(['players', state.get('turn')]).id,
+                        players: {
+                            me: {
+                                hand: state.getIn(['players', 1]).hand,
+                                money: state.getIn(['players', 1]).money
+                            },
+                            opponent: {
+                                money: state.getIn(['players', 0]).money
+                            }
+                        }
+                    });
+                                
+                } else {
+                    socket.emit('wait_for_opponent');
+                }
+
+            });
+
+            socket.on('disconnect', socket => {
+                clients--;
+                state = initialState;
+                io.sockets.emit('restart');
+            });
 
         });
 
---
+++ server/src/util/poker.js
@@ -0,0 +1,139 @@
+// Example implementation used in the Think Functional course
+
+//
+// Playing Cards class definition and implementation
+// in a functional fashiom
+//
+
+const _ = require('lodash');
+
+export const Ranks = Object.freeze([ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]);
+export const Suits = Object.freeze([ 'hearts', 'clubs', 'diams', 'spades' ]);
+
+const Cards = Object.freeze(Object.entries(Ranks).reduce(
+  (cards, [ weight, rank ]) => cards.concat(Suits.map(suit => ({ rank, suit, weight }))),
+  []
+));
+
+export class PlayingCards {
+ 
+  constructor(cards = null, from = 0, to = 0) {
+    const cardsSource = cards instanceof Array ? cards : Cards;
+    const cardsRange = cardsSource.slice(from, to || cardsSource.length);
+    
+    this.cards = Object.freeze(cardsRange.sort(() => Math.random() - 0.5));
+    
+    this.genStats();
+    
+    Object.freeze(this);
+  }
+
+  getNCardsAndRest(n) {
+    return {
+      cards: new PlayingCards(this.cards, 0, n),
+      restCards: new PlayingCards(this.cards, n, this.cards.length),
+    };
+  }
+
+  genStats() {    
+    this.ordCards = [...this.cards].sort((a, b) => a.weight - b.weight);
+    this.ranks = _.groupBy(this.ordCards, 'rank');
+    this.suits = _.groupBy(this.ordCards, 'suit');
+    this.rankTimes = _.groupBy(this.ranks, 'length');
+    this.suitTimes = _.groupBy(this.suits, 'length');
+    this.maxInARow = maxInARow(this.ordCards.map(({ weight }) => weight));
+  }
+
+  getOfSameRank(n) { return this.rankTimes[n] || []; }
+
+  getOfSameSuit(n) { return this.suitTimes[n] || []; }
+
+  hasAce() { return !!this.ranks['A']; }
+
+  hasOfSameRank(n) { return this.getOfSameRank(n).length; }
+
+  hasOfSameSuit(n) { return this.getOfSameSuit(n).length; }
+
+  hasInARow(n) { return this.maxInARow >= n; }
+
+  getWorstSingles() { return _.sortBy(_.flatten(this.getOfSameRank(1)), 'weight'); }
+
+  getHandRate(handRate) {
+    switch(handRate) {
+      case "Full House":
+        return {
+          handRate,
+          handWeight: _.flatten(this.rankTimes[3])[0].weight
+        };
+
+      case "One Pair":
+        return {
+          handRate,
+          handWeight: _.flatten(this.rankTimes[2])[0].weight
+        };
+      
+      case "Two Pair":
+        return {
+          handRate,
+          handWeight: _.flatten(this.rankTimes[2])[2].weight
+        };
+
+      case "Three of a Kind":
+        return {
+          handRate,
+          handWeight: _.flatten(this.rankTimes[3])[0].weight
+        };
+      
+      case "Four of a Kind":
+        return {
+          handRate,
+          handWeight: _.flatten(this.rankTimes[4])[0].weight
+        };
+
+      default:
+        return {
+          handRate,
+          handWeight: this.ordCards[4].weight
+        };
+    }
+  }
+
+}
+
+//
+// Poker Ratings
+//
+const PokerRating = {
+  "Royal Flush": (hand) => hand.hasInARow(5) && hand.hasOfSameSuit(5) && hand.hasAce(),
+  "Straight Flush": (hand) => hand.hasInARow(5) && hand.hasOfSameSuit(5),
+  "Four of a Kind": (hand) => hand.hasOfSameRank(4),
+  "Full House": (hand) => hand.hasOfSameRank(3) && hand.hasOfSameRank(2),
+  "Flush": (hand) => hand.hasOfSameSuit(5),
+  "Straight": (hand) => hand.hasInARow(5),
+  "Three of a Kind": (hand) => hand.hasOfSameRank(3),
+  "Two Pair": (hand) => hand.hasOfSameRank(2) >= 2,
+  "One Pair": (hand) => hand.hasOfSameRank(2),
+  "High Card": (hand) => hand.hasOfSameRank(1) >= 5,
+};
+
+const PokerHandRate = (playingCards) => Object.entries(PokerRating).find(([rate, is]) => is(playingCards))[0];
+
+export const PokerResults = (cards) => {
+  const playingCards = new PlayingCards(cards);
+  return playingCards.getHandRate(PokerHandRate(playingCards));
+}
+
+//
+// Max in a Row Utility
+//
+function maxInARow(nums) {
+  return _.chain(nums)
+    .sortBy()
+    .uniq()
+    .map((num, i) => (num - i))
+    .groupBy()
+    .orderBy('length')
+    .last()
+    .value()
+    .length;
+}
