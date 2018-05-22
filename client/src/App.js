4a38bbebeed9e5d280b7f09b04e673733d200230
Alex Alikiotis <alexalikiotis5@gmail.com>
2018-05-22 15:23:35 +0300

    Updating eslint react config

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 import Lobby from './modules/lobby/container';
 
+const App
+
 class App extends Component {
   render() {
     return (
-      <Lobby/>
+      <Lobby />
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -6,18 +6,20 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './redux/reducers';
 import SocketConnection from './util/socket';
+import Lobby from './modules/lobby/container';
 import './stylesheets/index.css';
 import './stylesheets/app.css';
 import './stylesheets/cards.css';
 
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-//const store = createStore(reducers);
+// const store = createStore(reducers);
 
 SocketConnection.open('http://localhost:8080', store);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
